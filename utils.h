17f29b746dc756f5aadbd384407d040712f84b10
Ruslan <ruslankhamroev@gmail.com>
2021-09-01 09:29:25 +0100

    added infix evaluation, added resources deallocation

--- utils.h
+++ utils.h
@@ -1,6 +1,7 @@
 #include <data_str.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <math.h>
 #ifndef MAX_STR_SIZE
 #define MAX_STR_SIZE 100
 #endif
@@ -15,6 +16,7 @@ char *convertToPostfix(char *string){
 	}
 	STACKPTR stack = new_stack();
 	char *postfix = malloc(MAX_STR_SIZE);
+	char *result = postfix;
 	//postfix[sizeof(string)-1]='\0';
 	push((int)'(',&stack);
 	//postfix[sizeof(string)-2]=')';
@@ -40,7 +42,51 @@ char *convertToPostfix(char *string){
 	}
 	++postfix=')';
 	++postfix='\0';
+	free(stack);
+	return result;
 }
+int evaluateInfixExpp(char *expp){
+	char *postfix = convertToPostfix(expp);
+	STACKPTR stack = new_stack();
+	while(!postfix){
+		if(isdigit(postfix)){
+			push((int)(postfix++ - '0'),&stack);
+		}
+		else if (isOperator(postfix))
+		{
+			int x,y;
+			if(!isEmpty(stack))x=pop(&stack);
+			if(!isEmpty(stack))y=pop(&stack);
+			switch(postfix++){
+				case'+':
+				push((int)(y+x),&stack);
+				break;
+				case'-':
+				push((int)(y-x),&stack);
+				break;
+				case'*':
+				push((int)(y*x),&stack);
+				break;
+				case'/':
+				push((int)(y/x),&stack);
+				break;
+				case'^':
+				push((int)(pow(y,x)),&stack);
+				break;
+				case'%':
+				push((int)(y%x),&stack);
+				break;
+			}
+		}
+	}
+	if(!isEmpty(stack)){
+		int result = pop(&stack);
+		free(stack);
+		return result;
+	}
+	return 0;
+}
+
 int isOperator(char ch){
 	return 		ch=='+'||
 				ch=='-'||
